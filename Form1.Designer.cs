b0740854377e3e87d68c3448ddff568ea4c1816b
nguyenpt <nguyenpt@bisync.jp>
2024-11-13 10:57:07 +0700

    [issue_001] Detect text

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,20 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.BtnClose = new System.Windows.Forms.Button();
+            this.BtnGetTextData = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 325);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(575, 113);
+            this.textBox1.TabIndex = 0;
+            // 
+            // BtnClose
+            // 
+            this.BtnClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.BtnClose.Location = new System.Drawing.Point(700, 415);
+            this.BtnClose.Name = "BtnClose";
+            this.BtnClose.Size = new System.Drawing.Size(88, 23);
+            this.BtnClose.TabIndex = 1;
+            this.BtnClose.Text = "Close";
+            this.BtnClose.UseVisualStyleBackColor = true;
+            this.BtnClose.Click += new System.EventHandler(this.BtnClose_Click);
+            // 
+            // BtnGetTextData
+            // 
+            this.BtnGetTextData.Location = new System.Drawing.Point(700, 386);
+            this.BtnGetTextData.Name = "BtnGetTextData";
+            this.BtnGetTextData.Size = new System.Drawing.Size(88, 23);
+            this.BtnGetTextData.TabIndex = 2;
+            this.BtnGetTextData.Text = "Get text data";
+            this.BtnGetTextData.UseVisualStyleBackColor = true;
+            this.BtnGetTextData.Click += new System.EventHandler(this.BtnGetTextData_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BtnGetTextData);
+            this.Controls.Add(this.BtnClose);
+            this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Extract tool";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button BtnClose;
+        private System.Windows.Forms.Button BtnGetTextData;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -4,9 +4,11 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace marcury_ext
 {
@@ -16,5 +18,75 @@ namespace marcury_ext
         {
             InitializeComponent();
         }
+
+        /// <summary>
+        /// Handle click close button
+        /// </summary>
+        /// <param name="sender">Sender</param>
+        /// <param name="e">EventArgs</param>
+        private void BtnClose_Click(object sender, EventArgs e)
+        {
+            // Quit application
+            Application.Exit();
+        }
+
+        /// <summary>
+        /// Handle click Get text data button
+        /// </summary>
+        /// <param name="sender">Sender</param>
+        /// <param name="e">EventArgs</param>
+        private void BtnGetTextData_Click(object sender, EventArgs e)
+        {
+            List<WinText> windows = new List<WinText>();
+            // Find the first "FormMarcury" window
+            IntPtr hWnd = FindWindow(null, "FormMarcury");
+            //while (hWnd != IntPtr.Zero)
+            //{
+            //    // Find the control window that has the text
+            //    IntPtr hEdit = FindWindowEx(hWnd, IntPtr.Zero, "edit", null);
+            //    //initialize the buffer.  using a StringBuilder here
+            //    System.Text.StringBuilder sb = new System.Text.StringBuilder(255);  // or length from call with GETTEXTLENGTH
+
+            //    //get the text from the child control
+            //    int RetVal = SendMessage(hEdit, WM_GETTEXT, sb.Capacity, sb);
+
+            //    windows.Add(new WinText() { hWnd = hWnd, Text = sb.ToString() });
+
+            //    //find the next window
+            //    hWnd = FindWindowEx(IntPtr.Zero, hWnd, "FormMarcury", null);
+            //}
+
+            //do something clever
+            windows.OrderBy(x => x.Text).ToList().ForEach(y => Console.Write("{0} = {1}\n", y.hWnd, y.Text));
+            if (hWnd != IntPtr.Zero)
+            {
+                Console.Write("\n\nFound FormMarcury window.");
+            }
+            Console.Write("\n\nFound {0} window(s).", windows.Count);
+        }
+        private struct WinText
+        {
+            public IntPtr hWnd;
+            public string Text;
+        }
+
+
+        const int WM_GETTEXT = 0x0D;
+        const int WM_GETTEXTLENGTH = 0x0E;
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern int SendMessage(IntPtr hWnd, int msg, int Param, System.Text.StringBuilder text);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string lpszClass, string lpszWindow);
+
+        internal delegate int WindowEnumProc(IntPtr hwnd, IntPtr lparam);
+
+        [DllImport("user32.dll")]
+        internal static extern bool EnumChildWindows(IntPtr hwnd, WindowEnumProc func, IntPtr lParam);
+
     }
 }
