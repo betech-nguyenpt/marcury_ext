aece7d9882092a9bdc17248d510be6dcb6a8354f
nguyenpt <nguyenpt@bisync.jp>
2024-12-11 08:23:39 +0700

    Init WPF project

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace marcury_wpf_prj
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
